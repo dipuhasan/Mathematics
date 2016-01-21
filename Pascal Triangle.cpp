12c8db779cc011276ae206c5ac359c162a3dd68d
dipuhasan <depu1994@gmail.com>
2016-01-22 00:24:08 +0600

    Create Pascal Triangle.cpp

---
+++ Pascal Triangle.cpp
@@ -0,0 +1,38 @@
+// A simple O(n^3) program for Pascal's Triangle
+#include <stdio.h>
+ 
+int binomialCoeff(int n, int k);
+
+void printPascal(int n)
+{
+  // Iterate through every line and print entries in it
+  for (int line = 0; line < n; line++)
+  {
+    for (int i = 0; i <= line; i++)
+      printf("%d ", binomialCoeff(line, i));
+    printf("\n");
+  }
+}
+ 
+int binomialCoeff(int n, int k)
+{
+    int res = 1;
+    if (k > n - k)
+       k = n - k;
+    for (int i = 0; i < k; ++i)
+    {
+        res *= (n - i);
+        res /= (i + 1);
+    }
+    return res;
+}
+ 
+
+int main()
+{
+  int n = 7;
+  printPascal(n);
+  return 0;
+}
+
+
